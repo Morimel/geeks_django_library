08cacbf4f4b7e3d73328705e18d0c856a3d48c4b
Morimel <melsovisa77@gmail.com>
2025-01-13 14:21:37 +0600

    сделал вторую домашку

--- library/models.py
+++ library/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    GENRE_CHOICES = (
+        ('Ужасы', 'Ужасы'),
+        ('Комедия', 'Комедия'),
+        ('Романтика', 'Романтика'),
+    )
+    image = models.ImageField(upload_to='books/', verbose_name='загрузите фото')
+    title = models.CharField(max_length=100, verbose_name='укажите название фильма')
+    description = models.TextField(verbose_name='напишите описание', blank=True, null=True)
+    price = models.PositiveIntegerField(verbose_name='укажите цену', default=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+    genre_choices = models.CharField(max_length=100, choices=GENRE_CHOICES, verbose_name='выберите жанр', default='Комедия')
+    director_email = models.EmailField(default='example@gmail.com', verbose_name='укажите email автора')
+    director = models.CharField(max_length=100, verbose_name='укажите режиссера', null=True)
+    trailer = models.URLField(verbose_name='вставьте ссылку с YouTube', default='https://example.com/trailer')
+    
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+        
+    def __str__(self):
+        return f'{self.title}'
--- library/urls.py
+++ library/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('book_detail/<int:id>/', views.book_detail, name='book_detail'),
     path('about-me/', views.about_me, name='about-me'),
     path('about-pet/', views.about_my_pets, name='about-pet'),
     path('time/', views.time, name='time')
